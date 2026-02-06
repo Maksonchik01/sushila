62a9b4a4ce36c04a4d80bcad9fc8f31146604777
Maksym_Demydasiuk <maksdem1406@gmail.com>
2026-02-06 13:02:11 +0100

    .

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+
+const bucketBtn = document.querySelector(".header-nav-btn-bucket")
+const bucket = document.querySelector(".header-nav-bucket")
+const closeBucketBtn = document.querySelector(".close-bucket")
 const button = document.querySelector(".btn")
 const span = document.querySelector(".span")
 const btn = document.querySelector(".btn-1")
 
+function openCard() {
+    bucket.classList.add("active-bucket")
+}
+bucketBtn.addEventListener("click", ()=> openCard())
+
+function closeCard() {
+    bucket.classList.remove("active-bucket")
+}
+closeBucketBtn.addEventListener("click", ()=>  closeCard())
 let count = 0;
 function countPlus() {
     count += 1
